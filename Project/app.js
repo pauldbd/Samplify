936d20e2cb9615ee76e4763fabcaf7478474a76e
pauldbd <loldeadmemegod@gmail.com>
2022-01-17 01:21:48 -0700

    set up canvas and get filtered data

--- Project/app.js
+++ Project/app.js
@@ -12,9 +12,14 @@ for (let i = 0; i < 16; i++){
     audios[i] = new Audio(); 
     const source = audioContext.createMediaElementSource(audios[i]); 
     source.connect(primaryGainCtrol); 
-}
+} 
 
-const masterGainInput = document.getElementById("masterGainInput"); 
+const audioVizualizationCanvas = document.getElementById("audioVizualization");
+let heightRatio = 0.2;
+audioVizualizationCanvas.height = audioVizualizationCanvas.width * heightRatio;
+const canvasContext = audioVizualizationCanvas.getContext("2d"); 
+canvasContext.translate(0, audioVizualizationCanvas.height/2); // Set Y = 0 to be in the middle of the canvas
+canvasContext.fillStyle = "rgb(92, 92, 91)";
 
 updateCurrentAudio(); 
 updateFile(); 
@@ -63,6 +68,9 @@ function updateCurrentAudio(){
         }); 
         file.addEventListener("change", function(){
             updateAudioName(i-1); 
+            if (!fileIsEmpty(i-1)){
+                visualizeAudio(i-1); 
+            }
         })
         
     }
@@ -93,8 +101,8 @@ async function getAudioBuffer(pad){
     return audioBuffer.getChannelData(0); 
 }
 
-function visualizeAudio(pad){
-    const rawData = getAudioBuffer(pad); 
+async function getData(pad){
+    const rawData = await(getAudioBuffer(pad)); 
     const samples = 70; 
     const blockSize = Math.floor(rawData.length / samples);
     let filteredData = [];
@@ -105,12 +113,23 @@ function visualizeAudio(pad){
         for (let j = 0; j < blockSize; j++){
             sum += Math.abs(rawData[j+i*samples]); 
         }
-        filteredData[i] = sum/blockSize; 
+        console.log(sum); 
+        filteredData.push(sum/blockSize); 
         max = Math.max(filteredData[i], max); 
     }
 
     const multiplyer = Math.pow(max, -1); 
     filteredData = filteredData.map(i => i * multiplyer); 
-
+    return filteredData; 
 
 }
+
+function visualizeAudio(pad){
+    // const filteredData = getData(pad); 
+
+    // for (let i = 0; i < filteredData.length; i++){
+    //     canvasContext.fillRect(i*2, 10*filteredData, 10, 20*filteredData)
+    // }
+
+}
+
